8cebe36de9aac0470898272b735f2ce8ef269aca
Smokey <ghostking2033@gmail.com>
2022-09-23 23:06:50 +0300

    laba1

--- PWSlaba/Controllers/HomeController.cs
+++ PWSlaba/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace PWSlaba.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult AboutUs()
         {
             return View();
         }
