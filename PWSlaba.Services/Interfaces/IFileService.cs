b300a915123ecf2712e412d196407e1a1446b564
Smokey <ghostking2033@gmail.com>
2022-11-11 22:44:21 +0200

    laba4

---
+++ PWSlaba.Services/Interfaces/IFileService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using PWSlaba.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PWSlaba.Services.Interfaces
+{
+    public interface IFileService
+    {
+        public List<FileModel> GetFilesList(string path);
+        public byte[] GetFileDownload(string path);
+        public Task<bool> UploadFile(IFormFile file, string savepath);
+    }
+}
--- PWSlaba/Controllers/HomeController.cs
+++ PWSlaba/Controllers/HomeController.cs
@@ -4,18 +4,25 @@ using PWSlaba.Models;
 using PWSlaba.Services.Interfaces;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using Microsoft.AspNetCore.Http;
 
 namespace PWSlaba.Controllers
 {
     public class HomeController : Controller
     {
         protected readonly IEmailSender _emailSender;
+        private readonly IFileService _fileService;
         private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public HomeController(ILogger<HomeController> logger, IEmailSender emailSender)
+        public HomeController(ILogger<HomeController> logger, IEmailSender emailSender, IWebHostEnvironment webHostEnvironment, IFileService fileService)
         {
             _logger = logger;
             _emailSender = emailSender;
+            _webHostEnvironment = webHostEnvironment;
+            _fileService = fileService;
         }
         [Route("home")]
         [Route("")]
@@ -44,6 +51,32 @@ namespace PWSlaba.Controllers
         {
             await _emailSender.SendEmailAsync(mailModel.To, mailModel.ToName, "Feedback message", mailModel.Body);
             return RedirectToAction("Index");
+        }
+        [Route("uploadfiles")]
+        public IActionResult UploadFiles()
+        {
+            var files = _fileService.GetFilesList(Path.Combine(this._webHostEnvironment.WebRootPath, "Files/"));
+
+            return View(files);
+        }
+
+        [HttpGet]
+        [Route("download-file")]
+        public FileResult DownloadFile(string filename)
+        {
+            string path = Path.Combine(this._webHostEnvironment.WebRootPath, "Files/") + filename;
+
+            var bytes = _fileService.GetFileDownload(path);
+
+            return File(bytes, "application/octet-stream", filename);
+        }
+        [HttpPost]
+        [Route("upload-file")]
+        public async Task<IActionResult> UploadFile(IFormFile File)
+        {
+            var res = await _fileService.UploadFile(File, "wwwroot/Files");
+
+            return RedirectToAction(nameof(UploadFiles));
         }
     }
 }
