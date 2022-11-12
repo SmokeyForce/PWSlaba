21eb64cf128b3c4a542f93d13c69dae0476fc6f4
Smokey <ghostking2033@gmail.com>
2022-11-12 15:53:25 +0200

    laba6

---
+++ PWSlaba/Components/NavbarViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PWSlaba.Components
+{
+    public class NavbarViewComponent : ViewComponent
+    {
+        private readonly IEnumerable<string> _menuElements;
+        public NavbarViewComponent() => _menuElements = new List<string>() { "Home", "AboutUs", "FeedBack", "UploadFiles" };
+        public IViewComponentResult Invoke()
+        {
+            return View(_menuElements);
+        }
+    }
+}
